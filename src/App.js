3e5bf6f8ddcb5ce3638365f862baf4e378b2ba50
Andres Santacruz <andres_v10@live.com>
2022-01-16 01:07:44 -0500

    [FIX]: Add context to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { UserContext } from "./components/contex/UserContext";
+import { auth } from "./firebase/firebase-config";
 import AppRouter from "./routers/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  console.log("Provider:  ", user);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
