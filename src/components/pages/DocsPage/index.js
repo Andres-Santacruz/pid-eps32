a913c67fad33acd4054af467abc67a85b6c1eb79
Andres Santacruz <andres_v10@live.com>
2022-06-02 01:38:31 -0500

    [UPDATE]: updated pages docs and tutorials.

--- src/components/pages/DocsPage/index.js
+++ src/components/pages/DocsPage/index.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import styles from "./styles.module.css";
 const DocsPage = () => {
   return (
-    <div>
-      <h1>DocsPage</h1>
+    <div className={styles.container}>
+      <div>
+        <h2 className={styles.title}>Como hacer un controlador PID con IoT</h2>
+        <p className={styles.text}>
+          Aquí está una guía para hacer un controlador PID con IoT a un sistema
+          viga - bola, utilizando herramientas de código abierto.
+        </p>
+        <img
+          src="https://0901.static.prezi.com/preview/v2/jazpgg7itmzbs3spefi623ybn36jc3sachvcdoaizecfr3dnitcq_3_0.png"
+          alt="sistema viga bola"
+          className={styles.img}
+        />
+      </div>
+      {/* crear lista de materiales */}
     </div>
   );
 };
--- src/components/pages/TutorialPage/index.js
+++ src/components/pages/TutorialPage/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import styles from "./styles.module.css";
 const TutorialPage = () => {
   return (
-    <div>
-      <h1>TutorialPage</h1>
+    <div className={styles.container}>
+      <div>
+        <h2 className={styles.title}>
+          Plataforma web para controlar un sistema viga - bola
+        </h2>
+        <p>
+          En esta guia podras ver el analisis de un sistema viga - bola y podras
+          aprender como utilizar el panel de control.
+        </p>
+        <img
+          src="https://i.stack.imgur.com/UNf2U.png"
+          alt="sistema viga bola"
+          className={styles.img}
+        />
+      </div>
     </div>
   );
 };
